401c99e5e7194dbbe84c3080a73a1a84fade1308
Jaime Casero <jaime.casero@telestax.com>
2017-09-18 13:41:34 -0400

    fixed https://github.com/RestComm/PerfTrictor/issues/1

--- src/main/java/org/restcomm/perftrictor/GenerateTrafficAction.java
+++ src/main/java/org/restcomm/perftrictor/GenerateTrafficAction.java
@@ -5,15 +5,16 @@ import org.squirrelframework.foundation.fsm.Action;
 
 public class GenerateTrafficAction implements Action<GlobalFSM, GlobalState, GlobalEvent, GlobalContext> {
 
+    private static long NANOS_IN_SEC = 1000000000;
     @Override
     public void execute(GlobalState s, GlobalState s1, GlobalEvent e, GlobalContext ctx, GlobalFSM t) {
         if (ctx.trafficFuture != null) {
             ctx.trafficFuture.cancel(false);
         }
-        long rate = 1000 / ctx.getIntegerProp("smppp.caps");
+        long rate = NANOS_IN_SEC / ctx.getIntegerProp("smppp.caps");
         ctx.logger.info("starting traffic with rate:" + rate);
         ctx.trafficFuture = ctx.executor.scheduleAtFixedRate(new TrafficTask(ctx), 0,
-                rate, TimeUnit.MILLISECONDS);       
+                rate, TimeUnit.NANOSECONDS);       
     }
 
     @Override
